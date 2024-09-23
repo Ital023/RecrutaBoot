d3c12a81341f476c1df77ed27cfb9d9aefc0bbe7
Ital023 <contadoitalo911@gmail.com>
2024-09-23 20:13:19 -0300

    feat: initial mobile content

---
+++ recrutaboot/android/app/src/main/kotlin/com/example/recrutaboot/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.recrutaboot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ recrutaboot/lib/app_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recruta Boot',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: 
+    );
+  }
+}
