b8db6d6508e7baadbdf4c94969ff9084e906dd31
Ital023 <contadoitalo911@gmail.com>
2024-09-24 16:26:45 -0300

    feat: Candidatos recentes homepage

--- recrutaboot/lib/routes/home_page.dart
+++ recrutaboot/lib/routes/home_page.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         iconTheme: const IconThemeData(
           color: Colors.white,
-          size: 30, 
+          size: 30,
         ),
         title: const Text("RecrutaBoot",
             style: TextStyle(fontSize: 25, color: Colors.white)),
@@ -27,18 +27,56 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           BannerHomePage(),
-          Container(
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.all(25),
+            color: Colors.grey[200],
+            child: Center(
+              child: Column(
+                children: [
+                  Text("Candidatos recentes",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(6)),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                            radius: 32,
+                            backgroundImage: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/113559117?v=4"),
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Italo Miranda", style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
+                              ),),
+                              Text("Desenvolvedor Mobile")
+                            ],
+                          ),
+                            ],
+                          ),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ))
+                ],
+              ),
             ),
-            child: Row(
-              children: [
-                Image.network("https://avatars.githubusercontent.com/u/113559117?v=4", height: 64, width: 64,)
-              ],
-            ),
-          )
+          ))
         ],
       ),
     );
