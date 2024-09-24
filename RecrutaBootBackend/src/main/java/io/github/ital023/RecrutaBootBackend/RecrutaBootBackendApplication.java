c214222bb88ba4af7ea171f3c346b4039d336b29
Ital023 <contadoitalo911@gmail.com>
2024-09-24 17:25:44 -0300

    feat: backend initial

---
+++ RecrutaBootBackend/src/main/java/io/github/ital023/RecrutaBootBackend/RecrutaBootBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.RecrutaBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutaBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutaBootBackendApplication.class, args);
+	}
+
+}
---
+++ RecrutaBootBackend/src/test/java/io/github/ital023/RecrutaBootBackend/RecrutaBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.RecrutaBootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutaBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
